e88562e09e53aa368391fe7db64042d4f4eb9c82
ElahehDaneshfar <elahehdaneshfar@gmail.com>
2022-05-16 10:11:56 -0500

    Astronomy

--- lab11-AstronomyPicture(AJAX Version)/astro.js
+++ lab11-AstronomyPicture(AJAX Version)/astro.js
@@ -1,39 +1,36 @@
 $(document).ready(
     function () {
         $("#view_button").click(getPicture);
-
     });
 
 function getPicture() {
-    const apiKey = 'jAO2cx8ytq76HPcjaf205dAYrrF0TIA49zrO7piK';
-    const dateValue = $("#date").val();
-    const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=` + `${dateValue}`;
-    fetchData(url);
+    const url = "https://api.nasa.gov/planetary/apod";
+    $.ajax({
+        url: url,
+        type: "GET",
+        data: {
+            api_key: "jAO2cx8ytq76HPcjaf205dAYrrF0TIA49zrO7piK",
+            date: $("#date").val()
+        },
+        dataType: "json",
+        "success": loadData,
+        "error": noPicture
+    });
 };
+
 function showPicture(data) {
     $("#pic").attr("src", data.url);
 };
+
 function noPicture(error) {
     alert(error.responseText);
-}
+};
 
 function showTitle(data) {
     $("#title").append(data.title);
-}
+};
 
 function loadData(data) {
     showPicture(data);
     showTitle(data);
-}
-
-function fetchData(url) {
-    fetch(url, {
-        method: "Get"
-    })
-        .then((res) => {
-            return res.json();
-        })
-        .then((data) => {
-            loadData(data);
-        })
-}
+};
